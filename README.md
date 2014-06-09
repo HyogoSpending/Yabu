c11d1e7c2350a65033a13a22cd07177271765429
HyogoSpending <HyogoSpending@gmail.com>
2014-06-09 10:09:42 +0900

    up

---
+++ README.md
@@ -0,0 +1,44 @@
+# WHERE DOES MY MONEY GO? for 養父
+
+wheredoesmymoneygo.orgの養父バージョンです．
+
+http://spending.jp/ で稼働しているサイトのソースコードです。
+
+養父版Where Does My Money Go? β版（2014年※月※※日稼働）は、養父市民が養父市に納めている市税を対象として構築したものです。自分の年間総収入をスライドで設定し、単身世帯か扶養一人世帯かを選択すると、給与所得者であるという前提で、養父市に納めている市税年総額と3分野毎に一日当たり支払っている市税額が表示されます。所得控除額は、単身世帯の場合33万円、扶養一人世帯の場合66万円、市税負担額は課税対象額の6％と設定しています。
+
+なお、このサイトは、公共データのオープン化によって、地域社会を変え、日本社会を変え、世界にも影響を与え、貢献して行こうという思いを持って集まった以下のメンバーによって構築、運営されています。
+
+
+Core Team: (@は Twitter Account)
+
+* 伊藤　大貴(Hirotaka Itou), [@hirochan](https://twitter.com/hirochan "twitter") (Adviser)  
+* 川島　宏一(Hiroichi Kawashima), [@hiroichi_k](https://twitter.com/hiroichi_k "twitter") (Coordinator)  
+* 佐藤　宏之(Hiroyuki Sato), [@sa2hi](https://twitter.com/sa2hi "twitter") (Linked Open Data化)  
+* 関　治之(Hal Seki:[@hal_sk](https://twitter.com/hal_sk "twitter")), Hack For Japan (Developer)  
+* 高木　祐介(Takagi Yuusuke:[@nyatakasan](https://twitter.com/nyatakasan "twitter")), (Developer)  
+* 高野　光弘(TAKANO Mitsuhiro), [@takano32](https://twitter.com/takano32 "twitter") (Developer)  
+* 竹内　久知(Takeuchi Hisatomo:[@3Dfalcon](https://twitter.com/3Dfalcon "twitter")), 機能型3Dアルバム (Designer)  
+* 田島　逸郎(Itsuro Tajima), [@niryuu](https://twitter.com/niryuu "twitter") (Developer)  
+* 玉木　光(Tamaki Hikaru), [Pearlpuppy.com](http://pearlpuppy.com/ "Pearlpuppy.com") (Designer)  
+* 西林　孝(Takashi Nishibayashi), [@hagino3000](https://twitter.com/hagino3000 "twitter") (Developer)  
+* 藤村　良弘(Fujimura Yoshihiro), [Facebook:Fujimura Yoshihiro](https://www.facebook.com/fujimura1969 "Facebook") (Policy Analyst)  
+
+
+養父市版
+Team Hyogo (Opendata Lab)
+
+* 榊原　貴倫(Takatomo Sakakibara) ,[Facebook](https://www.facebook.com/budo.brickworks "Facebook") , (Producer)  
+* 吉田　博哉(Hiroya Yoshida),[Facebook](https://www.facebook.com/hiroya.yoshida "Facebook") , (Director)  
+* 谷川　佳延(Yoshinobu Tanigawa),[Facebook](https://www.facebook.com/yoshinobu.tanigawa.7 "Facebook") , (Director)  
+* 新井　イスマイル(Ismail Arai),[Facebook](https://www.facebook.com/ismail.arai "Facebook") , (Director)  
+* 上田　信幸(Nobuyuki Ueda)  
+* 植松　亮(Ryo Uematsu)  
+* 梶田　成祥(Nariyoshi Kajita)  
+* 冨士　修一(Shuichi Fuji)  
+
+
+
+このソースコードは、[Open Definition](http://opendefinition.org/ "Open Definition") の定義に沿っていれば、どなたでも利用、改変、及び再配布が可能です。
+http://qiita.com/items/38c186efbd272ef59121
+に、このソースコードを Fork して独自のサイトを作る為のドキュメントがありますので、是非ご自由にお使いください。
+あなたのサイトを作成したら、是非御連絡や Pull Request をいただければと思います。
---
+++ js/setting.js
@@ -0,0 +1,51 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = '2012yabubudget';
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* yabu cofog */
+ /* '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-3': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/schools.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-1': { icon: 'icons/pre-school.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2-2': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#0AB971' },
+  '3': { icon: 'icons/economy-tourism.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-1': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-2': { icon: 'icons/harbor.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-3': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-4': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '3-5': { icon: 'icons/culture.svg', color: '#C75746', bcolor: '#4E6D00' },
+  */
+  
+  '2012100': { icon: 'icons/dollar.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2012101': { icon: 'icons/civilian-action.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2012102': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2012103': { icon: 'icons/government.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2012200': { icon: 'icons/transfers.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2012201': { icon: 'icons/construction2.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2012202': { icon: 'icons/ambulance.svg', color: '#C75746', bcolor: '#0AB971' },
+  '2012300': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '2012301': { icon: 'icons/housing.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '2012302': { icon: 'icons/island.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '2012303': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '2012304': { icon: 'icons/public-debt.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '2012305': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#4E6D00' },
+};
+
+
